47bbc95060e81887feb1f9fb3193cff7511a5100
Li Yuqing <im.yuqlee@gmail.com>
2021-10-28 13:32:59 +0800

    Test for account

--- account_test.go
+++ account_test.go
@@ -3,10 +3,16 @@ package api
 import (
 	"log"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAccount(t *testing.T) {
-	account, err := DecodeAccount("test account")
-	log.Println(err)
-	log.Printf("%#v", account)
+	assert := assert.New(t)
+	account, err := DecodeAccount("d9V5WDNZxa7fNRw24JwaqDpCrRnKjpsGcN2CpLJNJZQjt7Vhwsmm2w2CoY4g2u7vy5HFxL5S8uGUUogApWteXdgrd3GnowaFUWU1HefMdGK7fdhWEcznR9SacnddL2KA3NmEzgDRoqqwHzTv8cPXo5udtRAy4Q4xYsPTmXkcZTH212SNXudQwA6KfwUqS3aKvJFMLcr1iUmfupMikwVYcfboJ6i3gejGhua5BVX1GRhL2BRWMHhnRCThqicQAy")
+	assert.Nil(err)
+	address, err := account.B58Code()
+	assert.Nil(err)
+	log.Println(address)
+	assert.Equal(address, "d9V5WDNZxa7fNRw24JwaqDpCrRnKjpsGcN2CpLJNJZQjt7Vhwsmm2w2CoY4g2u7vy5HFxL5S8uGUUogApWteXdgrd3GnowaFUWU1HefMdGK7fdhWEcznR9SacnddL2KA3NmEzgDRoqqwHzTv8cPXo5udtRAy4Q4xYsPTmXkcZTH212SNXudQwA6KfwUqS3aKvJFMLcr1iUmfupMikwVYcfboJ6i3gejGhua5BVX1GRhL2BRWMHhnRCThqicQAy")
 }
