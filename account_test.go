d5f6fe2c586c4557ff9ba0f0e0f59e46e6c9fb8b
Li Yuqing <im.yuqlee@gmail.com>
2021-11-12 16:40:14 +0800

    Update package with more tests

--- account_test.go
+++ account_test.go
@@ -15,4 +15,11 @@ func TestAccount(t *testing.T) {
 	assert.Nil(err)
 	log.Println(address)
 	assert.Equal(address, "d9V5WDNZxa7fNRw24JwaqDpCrRnKjpsGcN2CpLJNJZQjt7Vhwsmm2w2CoY4g2u7vy5HFxL5S8uGUUogApWteXdgrd3GnowaFUWU1HefMdGK7fdhWEcznR9SacnddL2KA3NmEzgDRoqqwHzTv8cPXo5udtRAy4Q4xYsPTmXkcZTH212SNXudQwA6KfwUqS3aKvJFMLcr1iUmfupMikwVYcfboJ6i3gejGhua5BVX1GRhL2BRWMHhnRCThqicQAy")
+
+	addr := "G57w8Br44AYd6aEKfagTyLFvt4tTLhDdzGsX6PbYwfumwpjc1htSpWfoey2FLYNKMJA28q8YyqYb83dh66A7BTVA4XNZzXsNNUDv1nTmaw"
+	account, err = DecodeAccount(addr)
+	address, err = account.B58Code()
+	assert.Nil(err)
+	log.Println(address)
+	assert.Equal("G57w8Br44AYd6aEKfagTyLFvt4tTLhDdzGsX6PbYwfumwpjc1htSpWfoey2FLYNKMJA28q8YyqYb83dh66A7BTVA4XNZzXsNNUDv1nTmaw", address)
 }
