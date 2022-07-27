7a6fce40c7f97863135af7e332e541591eff2be2
LiYuqing <im.yuqlee@gmail.com>
2022-07-27 18:13:57 +0800

    More tests

--- account_test.go
+++ account_test.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"encoding/hex"
 	"log"
 	"testing"
 
+	"github.com/jadeydi/mobilecoin-account/block"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/proto"
 )
 
 func TestAccount(t *testing.T) {
@@ -22,4 +25,21 @@ func TestAccount(t *testing.T) {
 	assert.Nil(err)
 	log.Println(address)
 	assert.Equal("G57w8Br44AYd6aEKfagTyLFvt4tTLhDdzGsX6PbYwfumwpjc1htSpWfoey2FLYNKMJA28q8YyqYb83dh66A7BTVA4XNZzXsNNUDv1nTmaw", address)
+
+	account, err = DecodeAccount("3DsoPZDzxAtCCvwuNKvmSQcqHaa6w99U2dxjroC8DXZTvMM2LJdUHDXXDVQEf3vqew5Xzc3nw5jKEUMUuqvNREfV5GVCyiEvusZUFJeeRt3ykVEfAnp6suXZe15R9vvNKMBf2sTR2Bk6PiRrwyNNFFchY1kYkdCsH4dAsGJmFpRB4kGkhXANxFbK7r2K1FTPGpPxav31XXkCaXB3KcTVEEWptGNnEYrDKJdvJvwuMdkzv8")
+	assert.Nil(err)
+	log.Printf("1:::%#v", account)
+
+	account, err = DecodeAccount("CpJVvph9KVdpngREbnazkU1Z5XvmYNeDzQxhf9ft1agRZYCBk9gdfY489ibBAHPEtbFTmcrK4tdXa47bbsgffq21FRErBWAHje83S67XvcybFA5CVkwZghjm8GCWEZGyFqdaZiHB4B54A1GBnmeHGJra97oz7vQUGPyQNYZnrS3CY2Et6SRoAweDwemuukGKmTcBG1kaJKWRjRVVktzuBYbXPvSzx9edbk9UirbKRoZHyu")
+	assert.Nil(err)
+	log.Printf("2:: %#v", account)
+
+	var pa block.PublicAddress
+	buf, _ := hex.DecodeString("0a220a201c548b856c6b0400c2a99b2e918f267cca1bdee0367dc01258bd2ff9d8d2167412220a201e34eb6fc825788b7532943f1bef76554a8e2624e65190ec65572a29cd2e811c")
+	err = proto.Unmarshal(buf, &pa)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf(hex.EncodeToString(pa.ViewPublicKey.GetData()))
+	log.Printf(hex.EncodeToString(pa.SpendPublicKey.GetData()))
 }
